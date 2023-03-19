5f0ccf873f6ff824a026d990814d3288f15f31c0
EstellaSaveljeva221RDB176 <124592117+EstellaSaveljeva221RDB176@users.noreply.github.com>
2023-03-19 23:37:15 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -3,10 +3,14 @@
 def build_heap(data):
     swaps = []
     n = len(data)
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
+   
+    for i in range(n//2, -1, -1):
+        heap(data, i, n, swaps)
+        
+    return swaps
+
+def heap(data, i, n, swaps)
     
-    for i in range(n):
     mindex = i
     left = 2 * i + 1
     right = 2 * i + 2
@@ -16,36 +20,37 @@ def build_heap(data):
     if right <= n and data[right] < data[mindex]:
         mindex = right
     if i != mindex:
-        swaps.append((index, mindex))
-        data[index], data[mindex] = data[mindex], data[index]
-        
-    return swaps
+        swaps.append((i, mindex))
+        data[i], data[mindex] = data[mindex], data[i]
+        heap(data, i, n, swaps)
+    
 
 def main():
     
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
     print("[!] \tUse an input to choose files or input - F or I ?")
     textInput = input(":").upper()
-    
-    # first two tests are from keyboard, third test is from a file
+    if "F" in textInput:
+        print("[!] \tEnter file name or file path. For example '0'.")
+        fileName = "tests/" + input(">:: \t")
+        if 'a' in fileName:
+            print("[Err]: \tForbidden name")
+            return   
+        file = open(fileName, "r")
 
+        n = int(file.readline())
+        data = list(map(int, file.readline().split()))
+        assert len(data) == n
+        swaps = buildHeap(data)
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
-    assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
-    swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
+    elif "I" in textInput:
+        print("[!] \tEnter text below.")
+        n = int(input(">:: \t"))
+        data = list(map(int, input(">:: \t").split()))
+        assert len(data) == n
+        swaps = buildHeap(data)
+    else:
+        print("[Err]:\tWrong input")
+    pass
     # output all swaps
     print(len(swaps))
     for i, j in swaps:
